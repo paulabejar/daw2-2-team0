4bb4b44f0de44173e9dfdc0e07d51de40d6a272b
kikezaporta <116558122+kikezaporta@users.noreply.github.com>
2024-01-22 14:02:49 +0100

    Borro ejemplo

--- mvc_completo_23-24/App/vistas/inc/footer.php
+++
@@ -1,4 +0,0 @@
-    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js "></script>
-    <script src="<?php echo RUTA_URL?>/js/main.js"></script>
-</body>
-</html>
--- mvc_completo_23-24/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //Cargamos el iniciador
-    require_once "../App/iniciador.php";
-
-    //Instanciamos la clase Core
-    $iniciar = new Core;
