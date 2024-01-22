120b166fba3d962e97356ec0048f6f9fb67023de
kikezaporta <116558122+kikezaporta@users.noreply.github.com>
2024-01-22 13:58:49 +0100

    He cambaido ka linea 6

--- mvc_completo_23-24/App/iniciador.php
+++ mvc_completo_23-24/App/iniciador.php
@@ -3,6 +3,6 @@
     require_once "config/configurar.php";
     require_once "helpers/funciones.php";
 
-    require_once "librerias/Base.php";
+    require_once "librerias/Bae.php";
     require_once "librerias/Controlador.php";
     require_once "librerias/Core.php";
