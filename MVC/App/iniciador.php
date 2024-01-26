2193e943da8c62b955d2e93cd6a8191f391f7500
oscarespada20 <132060065+oscarespada20@users.noreply.github.com>
2024-01-26 10:55:19 +0100

    Delete MVC directory

--- MVC/App/iniciador.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Cargamos librerías
-    require_once "config/configurar.php";
-    require_once "helpers/funciones.php";
-
-    require_once "librerias/Base.php";
-    require_once "librerias/Controlador.php";
-    require_once "librerias/Core.php";
-    require_once "librerias/Sesion.php";
