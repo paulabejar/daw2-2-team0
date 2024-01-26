3a5b7febc5e043074f10dbe4b098097f4b3f0a58
paulabejar <paulabejar@example.com>
2024-01-26 10:00:59 +0000

    Header cambiado

--- MVC_v0/App/vistas/inc/header_no_logueado.php
+++ MVC_v0/App/vistas/inc/header_no_logueado.php
@@ -4,15 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <title>Header sin loguearse</title>
 </head>
 <body>
-    <div class="container-fluid mt-1">
-        <div class="d-flex justify-content-between">
-            <a href="#"><img class="img mt-1" src="<?php echo RUTA_URL ?>/img/logoBA.png" alt="logoBA" style="width: 150px;"></a>
-            <button class="btn">Inciar sesión</button>
+    <nav class="navbar">
+        <div class="container-fluid">
+            <a href="#"><img class="img mt-1" src="/img/logoBA.png" alt="logoBA" style="width: 150px;"></a>
+            <button class="btn" type="submit">Iniciar Sesión</button>
         </div>
-    </div>
-</body>
-</html>
+    </nav>
