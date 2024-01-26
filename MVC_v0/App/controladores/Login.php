6191213500736989034ddefe72fdb90e81f9358b
paulabejar <paulabejar@example.com>
2024-01-26 12:24:39 +0000

    fixed-top en header y creación de login

---
+++ MVC_v0/App/controladores/Login.php
@@ -0,0 +1,10 @@
+<?php
+    class Login extends Controlador {
+
+        public function __construct() {
+        }
+
+        public function index() {
+            $this->vista('login');
+        }
+    }
