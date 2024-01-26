ab19326069eee40ab521ff019ed8b6cd2e1b6707
paulabejar <paulabejar@example.com>
2024-01-26 13:38:12 +0000

    footer

--- MVC_v0/App/vistas/inc/footer.php
+++ MVC_v0/App/vistas/inc/footer.php
@@ -1,13 +1,9 @@
 
-<div class="container-fluid fixed-bottom">
-    <div class="row">
-        <div class="d-flex align-items-center col-12 bg-secondary h-25 p-1">
-            <p>Política de cookies</p>
-        </div>
+<footer class="container-fluid bg-secondary fixed-bottom">
+    <div>
+        <a class="link-dark" href="#"><p>Política de cookies</p></a>
     </div>
-    
-       
-</div>
+</footer>
     <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js "></script>
     <script src="<?php echo RUTA_URL?>/js/main.js"></script>
 </body>
