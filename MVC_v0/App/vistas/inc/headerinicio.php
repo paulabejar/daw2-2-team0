0407fff58bcc21922112f307c41a32367fec7ae7
paulabejar <118204787+paulabejar@users.noreply.github.com>
2024-01-29 11:30:01 +0100

    Update headerinicio.php

--- MVC_v0/App/vistas/inc/headerinicio.php
+++ MVC_v0/App/vistas/inc/headerinicio.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="img/icon.ico">
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    
+    <title>BAJO ARAGON</title>
+</head>
+<body  id="inicio">
+    <nav class="navbar bg-light fixed-top">
+        <div class="container-fluid me-3 ms-3">
 
+            <a href="<?php echo RUTA_URL ?>"><img class="img" src="/img/logoBA.png" alt="logoBA" style="width: 180px;"></a>
+            <a class="link-opacity-50-hover link-body-emphasis link-underline-opacity-0 link-underline-opacity-25-hover " href="<?php echo RUTA_URL ?>/login">Iniciar Sesión</a>
+            
+        </div>
+    
+    </nav>
